ae75d36a4580501efd6f0c29979e0389f90a0c01
Alberto Laita <alberto.laita@ocado.com>
2020-02-28 10:45:13 +0100

    fix error with home endpoint

--- safaribooks/spiders/safaribooks.py
+++ safaribooks/spiders/safaribooks.py
@@ -90,7 +90,7 @@ class SafariBooksSpider(scrapy.spiders.Spider):
         if self.cookie is not None:
             cookies = dict(x.strip().split('=') for x in self.cookie.split(';'))
 
-            return scrapy.Request(url=self.host + 'home',
+            return scrapy.Request(url=self.host + '/home',
                 callback=self.after_login,
                 cookies=cookies,
                 headers={
